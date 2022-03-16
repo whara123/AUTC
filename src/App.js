f281becd54f7b2c0f0560c6e2e8021f9dbf8d6fe
whara123 <64121533+whara123@users.noreply.github.com>
2022-03-16 18:38:32 +0900

    🔥 Remove: 중복 내용 제거

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
-import Home from "./pages/Home/Home";
-import GlobalStyle from "./styles/GlobalStyle";
 
 export default function App() {
   return (
     <div>
-      <GlobalStyle />
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
+      <h1>app</h1>
     </div>
   );
 }
