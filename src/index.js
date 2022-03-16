d5d91a2f58aba71f0b709d7a5c511f9ef4ae2c26
whara123 <64121533+whara123@users.noreply.github.com>
2022-03-16 18:39:13 +0900

    🎨 Improve: app/index 간 중복 내용 병합

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import App from "./App";
+import Create from "./pages/Create/Create";
+import Home from "./pages/Home/Home";
+import GlobalStyle from "./styles/GlobalStyle";
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
+      <GlobalStyle />
       <Routes>
-        <Route path="/" element={<App />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/create" element={<Create />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
