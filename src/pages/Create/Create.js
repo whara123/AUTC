48effb2ed5af670b462b7eddec856e8149729586
whara123 <64121533+whara123@users.noreply.github.com>
2022-03-23 18:24:57 +0900

    :white_check_mark: Modify createpage

--- src/pages/Create/Create.js
+++ src/pages/Create/Create.js
@@ -1,55 +1,38 @@
 import React, { useState } from "react";
 import styled from "styled-components";
 import Header from "../../components/Header";
-import SelectItem from "./SelectItem";
 
 export default function Create() {
   const [genre, setGenre] = useState("장르");
-  const [contentName, setContentName] = useState("");
   const [content, setContent] = useState("컨텐츠");
 
   const handleGenre = (e) => {
     setGenre(e.target.value);
   };
 
-  const handlecontent = (e) => {
+  const handleContent = (e) => {
     setContent(e.target.value);
   };
-  const fakeData = ["로비", "뽑기", "pve", "pvp", "상점", "영웅", "친구", "장비", "업적"];
 
   return (
     <CreateBody>
       <Header pageName="TestCase 생성 페이지" />
-      <MainWrap>
-        <SelectWrap>
-          <p>장르 선택</p>
-          <SelectList>
-            <SelectItem name="수집형RPG" btnType="button" onClick={handleGenre} />
-            <SelectItem name="MMORPG" btnType="button" onClick={handleGenre} />
-          </SelectList>
-          <Titles>{genre}</Titles>
-        </SelectWrap>
+      <main>
+        <WrapCreateSelect>
+          <SelectOption value={genre} onChange={handleGenre}>
+            <option value="수집형 RPG">수집형 RPG</option>
+          </SelectOption>
 
-        <SelectWrap>
-          <p>컨텐츠 선택</p>
-          <SelectList>
-            {fakeData.map((data) => (
-              <SelectItem key={`list_${data}`} name={data} btnType="button" onClick={handlecontent} />
-            ))}
-          </SelectList>
-          <Titles>{content}</Titles>
-        </SelectWrap>
+          <SelectOption value={content} onChange={handleContent}>
+            <option value="로비">로비</option>
+            <option value="모험">모험</option>
+            <option value="던전">던전</option>
+            <option value="소환">소환</option>
+          </SelectOption>
 
-        <WrapInput>
-          <input
-            type="text"
-            value={contentName}
-            onChange={(e) => setContentName(e.target.value)}
-            placeholder="T.C 제목입력"
-          />
-          <CreateButton type="button">생성</CreateButton>
-        </WrapInput>
-      </MainWrap>
+          <CreateBtn type="button">생성</CreateBtn>
+        </WrapCreateSelect>
+      </main>
     </CreateBody>
   );
 }
@@ -60,61 +43,19 @@ const CreateBody = styled.div`
   overflow: hidden;
 `;
 
-const MainWrap = styled.main`
-  display: flex;
-  width: 100%;
-  height: 100%;
-  gap: 5em;
-  justify-content: center;
-  align-items: center;
+const SelectOption = styled.select`
+  padding: 5px;
+  font-size: 20px;
+  box-sizing: border-box;
 `;
 
-const SelectWrap = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-`;
-
-const SelectList = styled.ul`
-  width: 10em;
-  height: 10em;
+const CreateBtn = styled.button`
   padding: 10px;
-  border: 1px solid #eee;
+  background-color: #8fdbbf;
   border-radius: 5px;
-  overflow-y: scroll;
-  &::-webkit-scrollbar {
-    width: 10px;
-  }
-  &::-webkit-scrollbar-thumb {
-    background-color: #2f3542;
-    border-radius: 10px;
-    background-clip: padding-box;
-    border: 2px solid transparent;
-  }
-  &::-webkit-scrollbar-track {
-    background-color: grey;
-    border-radius: 10px;
-    box-shadow: inset 0px 0px 5px white;
-  }
 `;
 
-const Titles = styled.p`
-  margin-top: 30px;
-  font-weight: bold;
-  font-size: 2em;
-`;
-
-const CreateButton = styled.button`
-  padding: 10px;
-  border-radius: 5px;
-  background: #c6edc8;
-`;
-
-const WrapInput = styled.form`
+const WrapCreateSelect = styled.div`
   display: flex;
-  justify-content: center;
-  align-items: center;
-  gap: 5em;
-  margin-bottom: 70px;
+  gap: 20px;
 `;
